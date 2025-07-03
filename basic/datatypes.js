ced107186da256abde2f5b7363f27a5363839ad6
ZaidMirkar <139256076+ZaidMirkar@users.noreply.github.com>
2025-07-03 17:55:08 +0000

    03-07-25

---
+++ basic/datatypes.js
@@ -0,0 +1,12 @@
+"use strict"//new ver of js
+//alert( 4+ 4) not allowed bcoz in node js not a browser
+console.log(3+3)
+console.log("z");
+//types number bigint string ""  boolean null undefined symbol
+
+
+// object
+console.log(typeof "z");
+console.log(typeof "null");
+console.log(typeof null);
+
---
+++ basic/variables.js
@@ -0,0 +1,14 @@
+const accId = 10279
+let accountEmail = "zaidmirkar55@gmail.com"
+var accCity = "Muscat"
+password = 0
+let car = 1
+
+password = 1
+accCity = "Mumbai"
+accountEmail = ".com"
+
+
+console.log(accId);
+console.table([accId , accountEmail , accCity , password , car ])
+// const aur let use kro becoz of issue in block scope and functional scope
