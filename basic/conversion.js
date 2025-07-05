6ad1e4bc2f6c64048c74a7bf057840b0ba77099b
ZaidMirkar <139256076+ZaidMirkar@users.noreply.github.com>
2025-07-05 11:40:14 +0000

    summry

--- basic/conversion.js
+++ basic/conversion.js
@@ -7,3 +7,21 @@ let bike = Number(z)
 console.log(typeof bike);
 console.log(bike);
 
+console.log("3" + 8 +9);
+console.log(3+5+"8");
+
+
+console.log(+true)
+
+
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+//comparison convert null to number equality treat it as null
+
+//===
+console.log("2"===2);
+
+//cleaen code
