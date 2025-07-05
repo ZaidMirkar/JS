6079f6b6657d360bcbbeb2f142e338917548f349
ZaidMirkar <139256076+ZaidMirkar@users.noreply.github.com>
2025-07-05 13:09:08 +0000

    summ

---
+++ basic/05summ.js
@@ -0,0 +1,17 @@
+// two types primitive and non primitive 
+//memory me kaise store aur access hote us basis pe do categorization h
+
+//PRIMITIVE STRING CALL BY VALUE COPY JAATI H
+/* NUMMBER null nan undefined symbol BigInt
+
+reference type ya NON PRIMITIVE
+ARRAY OBJECTS FUNCTIONS*/
+let myObj = { 
+             name:" zaid",
+             age :19}
+             const hheroes = ["zaid" , "azim" , ]
+             console.log(myObj);
+             console.log(hheroes);
+           //smjha nai  console.table(myObj,hheroes)
+             
+             
